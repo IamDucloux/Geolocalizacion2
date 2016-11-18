6184601ace54367b3e5b2d06fb2206c9974650aa
T-107 <ianducloux@gmail.com>
2016-11-18 16:41:36 -0600

    ian

---
+++ app/src/main/java/unitec/org/geolocalizacion2/MainActivity.java
@@ -0,0 +1,27 @@
+package unitec.org.geolocalizacion2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.boton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+    }
+}
